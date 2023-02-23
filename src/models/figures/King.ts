0a623edd53c6100caeddd350830d2b198b1d3e98
Nikita <91351927+LitvinovNikita@users.noreply.github.com>
2023-02-22 19:25:54 -0500

    Update King movement

--- src/models/figures/King.ts
+++ src/models/figures/King.ts
@@ -18,8 +18,21 @@ export class King extends Figure{
 
 
     canMove(target: Cell): boolean {
-        if(!super.canMove(target))
-            return false;
-        return true;
+        if(target.figure?.name === FigureNames.KING && target.figure?.color) console.log(target);
+        if(!super.canMove(target)) return false;
+        const isVerticalMove = (target.y === this.cell.y + 1 || target.y === this.cell.y - 1) && target.x === this.cell.x;
+        const isHorizontalMove = (target.x === this.cell.x + 1 || target.x === this.cell.x - 1) && target.y === this.cell.y;
+        const isLeftDiagonal = (
+          (target.x === this.cell.x + 1 && target.y === this.cell.y + 1) 
+          || (target.x === this.cell.x - 1 && target.y === this.cell.y - 1)
+        )
+        const isRightDiagonal = (
+          (target.x === this.cell.x + 1 && target.y === this.cell.y - 1) 
+          || (target.x === this.cell.x - 1 && target.y === this.cell.y + 1)
+        )
+    
+        if(isVerticalMove || isHorizontalMove || isLeftDiagonal || isRightDiagonal) return true;
+    
+        return false;
     }
 }
